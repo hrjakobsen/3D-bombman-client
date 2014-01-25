414cd6b842e2b1ae6a73780f71910f32a037193f
jbcoding <mads12bjoern@gmail.com>
2014-01-25 07:54:47 -0800

    Add standart code for client

--- 3D-bombman-client/main.cpp
+++ 3D-bombman-client/main.cpp
@@ -1,1 +1,228 @@
-DØ MADS
+//
+// chat_client.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
+#include <deque>
+#include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+#include <boost/format.hpp>
+#include "chat_message.hpp"
+#include <time.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using boost::asio::ip::tcp;
+
+typedef std::deque<chat_message> chat_message_queue;
+
+std::string now_str()
+{
+	// Get current time from the clock, using microseconds resolution
+	const boost::posix_time::ptime now =
+		boost::posix_time::microsec_clock::local_time();
+
+	// Get the time offset in current day
+	const boost::posix_time::time_duration td = now.time_of_day();
+
+	//
+	// Extract hours, minutes, seconds and milliseconds.
+	//
+	// Since there is no direct accessor ".milliseconds()",
+	// milliseconds are computed _by difference_ between total milliseconds
+	// (for which there is an accessor), and the hours/minutes/seconds
+	// values previously fetched.
+	//
+	const long hours = td.hours();
+	const long minutes = td.minutes();
+	const long seconds = td.seconds();
+	const long milliseconds = td.total_milliseconds() -
+		((hours * 3600 + minutes * 60 + seconds) * 1000);
+
+	//
+	// Format like this:
+	//
+	//      hh:mm:ss.SSS
+	//
+	// e.g. 02:15:40:321
+	//
+	//      ^          ^
+	//      |          |
+	//      123456789*12
+	//      ---------10-     --> 12 chars + \0 --> 13 chars should suffice
+	//
+	//
+	char buf[40];
+	sprintf(buf, "%02ld:%02ld:%02ld.%03ld",
+		hours, minutes, seconds, milliseconds);
+
+	return buf;
+}
+
+class chat_client
+{
+public:
+	chat_client(boost::asio::io_service& io_service,
+		tcp::resolver::iterator endpoint_iterator)
+		: io_service_(io_service),
+		socket_(io_service)
+	{
+		do_connect(endpoint_iterator);
+	}
+
+	void write(const chat_message& msg)
+	{
+		io_service_.post(
+			[this, msg]()
+		{
+			bool write_in_progress = !write_msgs_.empty();
+			write_msgs_.push_back(msg);
+			if (!write_in_progress)
+			{
+				do_write();
+			}
+		});
+	}
+
+	void close()
+	{
+		io_service_.post([this]() { socket_.close(); });
+	}
+
+private:
+	void do_connect(tcp::resolver::iterator endpoint_iterator)
+	{
+		boost::asio::async_connect(socket_, endpoint_iterator,
+			[this](boost::system::error_code ec, tcp::resolver::iterator)
+		{
+			if (!ec)
+			{
+				do_read_header();
+			}
+		});
+	}
+
+	void do_read_header()
+	{
+		boost::asio::async_read(socket_,
+			boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+			[this](boost::system::error_code ec, std::size_t /*length*/)
+		{
+			if (!ec && read_msg_.decode_header())
+			{
+				do_read_body();
+			}
+			else
+			{
+				socket_.close();
+			}
+		});
+	}
+
+	void do_read_body()
+	{
+		boost::asio::async_read(socket_,
+			boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+			[this](boost::system::error_code ec, std::size_t /*length*/)
+		{
+			if (!ec)
+			{
+				std::cout.write(read_msg_.body(), read_msg_.body_length());
+				std::cout << " - " << now_str() << "\n";
+				do_read_header();
+			}
+			else
+			{
+				socket_.close();
+			}
+		});
+	}
+
+	void do_write()
+	{
+		boost::asio::async_write(socket_,
+			boost::asio::buffer(write_msgs_.front().data(),
+			write_msgs_.front().length()),
+			[this](boost::system::error_code ec, std::size_t /*length*/)
+		{
+			if (!ec)
+			{
+				write_msgs_.pop_front();
+				if (!write_msgs_.empty())
+				{
+					do_write();
+				}
+			}
+			else
+			{
+				socket_.close();
+			}
+		});
+	}
+
+private:
+	boost::asio::io_service& io_service_;
+	tcp::socket socket_;
+	chat_message read_msg_;
+	chat_message_queue write_msgs_;
+};
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		std::cout << "Server IP:\n";
+		std::string IP;
+		std::cin >> IP;
+
+		std::cout << "Server PORT:\n";
+		std::string PORT;
+		std::cin >> PORT;
+
+		if (argc != 3)
+		{
+			//std::cerr << "Usage: chat_client <host> <port>\n";
+			//return 1;
+		}
+
+		boost::asio::io_service io_service;
+
+		tcp::resolver resolver(io_service);
+		auto endpoint_iterator = resolver.resolve({ IP, PORT });
+		chat_client c(io_service, endpoint_iterator);
+
+		std::thread t([&io_service](){ io_service.run(); });
+
+		char line[chat_message::max_body_length + 1];
+		while (std::cin.getline(line, chat_message::max_body_length + 1))
+		{
+			chat_message msg;
+			msg.body_length(std::strlen(line));
+			std::memcpy(msg.body(), line, msg.body_length());
+			msg.encode_header();
+
+
+			std::cout << now_str() << "\n";
+			c.write(msg);
+
+		}
+
+		c.close();
+		t.join();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
+}
