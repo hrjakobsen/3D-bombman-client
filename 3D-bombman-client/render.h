de02570133d92c9b0b638b2c20baf980068e4206
jbcoding <mads12bjoern@gmail.com>
2014-01-26 12:22:11 -0800

    Nothing

--- 3D-bombman-client/render.h
+++ 3D-bombman-client/render.h
@@ -87,7 +87,7 @@ void display(void) {
 	glBindTexture(GL_TEXTURE_2D, Tex->textureID);
 
 	glBegin(GL_QUADS);
-	glColor3f(1, 1, 1);
+	//glColor3f(1, 1, 1);
 
 	glTexCoord2f(10, 10);
 	glVertex3f(10, 0, 10);
