16cd9014e067dbf6af937d96a2b6e83c1f06dacc
jbcoding <mads12bjoern@gmail.com>
2014-01-26 23:11:45 -0800

    Maks cap on server update

--- 3D-bombman-client/main.cpp
+++ 3D-bombman-client/main.cpp
@@ -260,7 +260,7 @@ private:
 					}
 				}
 
-				std::this_thread::sleep_for(std::chrono::milliseconds(20));
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
 				string SendText = (MyPID + ";" + tostr(BodyPosition.x) + ";" + tostr(BodyPosition.z));
 				if (PlaceNextTime) {
 					PlaceNextTime = false;
